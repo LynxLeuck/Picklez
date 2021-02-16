c995a3e8f409cc802361c6433c6ad036a22e0987
Matt Maynard <maynard1488@gmail.com>
2021-02-16 00:20:41 -0500

    Starting over again. Most of the basic components have been added. Still need to add radio buttons.

--- src/main/java/com/picklez/SearchScreen.java
+++ src/main/java/com/picklez/SearchScreen.java
@@ -1,2 +1,15 @@
-package com.picklez;public class SearchScreen {
+package com.picklez;
+
+import javax.swing.*;
+
+public class SearchScreen extends JFrame {
+
+    private JLabel lblTitle;
+    private JTextField inputText;
+    private JTextArea resultsTextArea;
+    private JButton searchButton;
+    private JButton maintenanceButton;
+    private JButton aboutButton;
+    private JLabel searchTermsLabel;
+    private JLabel numberOfFilesLabel;
 }
