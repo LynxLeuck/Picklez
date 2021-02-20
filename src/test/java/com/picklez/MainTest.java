9bf18775c94067a02f7ee29c13711e2c232e29ca
Matt Maynard <maynard1488@gmail.com>
2021-02-20 17:18:57 -0500

    Adding JUnit 5
    Added JUnit 5 to dependencies in pom.xml and created a skeleton test class for Main

---
+++ src/test/java/com/picklez/MainTest.java
@@ -0,0 +1,18 @@
+package com.picklez;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+}
