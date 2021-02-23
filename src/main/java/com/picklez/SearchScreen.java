ffdc0a9bf67afc189ebdf51b257995506838d611
ulzkoca <ulzkoca@users.noreply.github.com>
2021-02-23 00:40:10 -0500

    Added SearchEngineMaintenance class, update SearchScreen class, added Java.class and updated pom to add JUnit5 dependencies.

--- src/main/java/com/picklez/SearchScreen.java
+++ src/main/java/com/picklez/SearchScreen.java
@@ -1,6 +1,8 @@
 package com.picklez;// Set up the GUI for the com.picklez.Main search screen
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class SearchScreen extends JFrame {
     private JButton maintenanceButton;
@@ -14,11 +16,16 @@ public class SearchScreen extends JFrame {
     private JRadioButton searchOption2;
     private JRadioButton searchOption3;
     private JPanel mainPanel;
+    private SearchEngineMaintenance searchEngineMaintenance = new SearchEngineMaintenance();
 
     public SearchScreen() {
         add(mainPanel);
         setTitle("Search Engine");
         setSize(780, 525);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        maintenanceButton.addActionListener(e -> searchEngineMaintenance.setVisible(true));
+        aboutButton.addActionListener(e -> {
+            JOptionPane.showMessageDialog(null, "Hello world!", "About Search Engine", JOptionPane.PLAIN_MESSAGE);
+        });
     }
 }
---
+++ src/main/java/com/picklez/Test.java
@@ -0,0 +1,14 @@
+package com.picklez;
+import java.io.*;
+
+public class Test {
+    public static void test(String[] args) {
+        helloWorld();
+
+    }
+    public static void helloWorld() {
+        System.out.println("Hello");
+    }
+}
+
+
